40f0aa49653e9d6147cd6c98d6a019281678187f
jim <623218869@qq.com>
2025-08-13 11:34:18 +0800

    fix: 支持多文件生成

--- process/process.go
+++ process/process.go
@@ -18,49 +18,53 @@ func processDefault(g *generator.Generator, f *generator.File, mapValue *generat
 	endpointPackage := generator.CreateImportWithPath(path.Join(convertedPath, "endpoint"))
 	basePackage := generator.CreateImportWithPath(convertedPath)
 
-	var buf bytes.Buffer
+	// 遍历所有模板配置
+	for _, templateConfig := range config.Config.Template {
+		var buf bytes.Buffer
 
-	templatePath := config.Config.Template[0].Templ
-	outFilename := config.Config.Template[0].Out
+		templatePath := templateConfig.Templ
+		outFilename := templateConfig.Out
 
-	var pathBuf bytes.Buffer
-	pathTmpl, err := template.New("path").Parse(outFilename)
-	if err != nil {
-		log.Fatalf("Path Template Parsing Error: %s", err)
-	}
-
-	outFilename = pathBuf.String()
-
-	files := []string{
-		templatePath,
-	}
-
-	tmpl, err := template.ParseFiles(files...)
-	if err != nil {
-		log.Fatalf("Template Parsing Error: %s", err)
-	}
-
-	for _, s := range f.Structs {
-		data := generator.CreateTemplateBase(basePackage, endpointPackage, generator.Interface{}, s, f.Imports)
-		err = tmpl.Execute(&buf, data)
-		if err != nil {
-			log.Fatalf("Template execution failed: %s\n", err)
-		}
-
-		err = pathTmpl.ExecuteTemplate(&pathBuf, "path", data)
+		var pathBuf bytes.Buffer
+		pathTmpl, err := template.New("path").Parse(outFilename)
 		if err != nil {
 			log.Fatalf("Path Template Parsing Error: %s", err)
 		}
 
-		outFilename = pathBuf.String()
+		files := []string{
+			templatePath,
+		}
 
-		func() {
-			file := utils.OpenFile(filepath.Dir(outFilename), filepath.Base(outFilename))
-			defer file.Close()
+		tmpl, err := template.ParseFiles(files...)
+		if err != nil {
+			log.Fatalf("Template Parsing Error: %s", err)
+		}
 
-			fmt.Fprint(file, string(utils.FormatBuffer(buf, outFilename)))
-		}()
+		for _, s := range f.Structs {
+			data := generator.CreateTemplateBase(basePackage, endpointPackage, generator.Interface{}, s, f.Imports)
+			err = tmpl.Execute(&buf, data)
+			if err != nil {
+				log.Fatalf("Template execution failed: %s\n", err)
+			}
 
+			err = pathTmpl.ExecuteTemplate(&pathBuf, "path", data)
+			if err != nil {
+				log.Fatalf("Path Template Parsing Error: %s", err)
+			}
+
+			outFilename = pathBuf.String()
+
+			func() {
+				file := utils.OpenFile(filepath.Dir(outFilename), filepath.Base(outFilename))
+				defer file.Close()
+
+				fmt.Fprint(file, string(utils.FormatBuffer(buf, outFilename)))
+			}()
+
+			// 清空buffer为下一个模板做准备
+			buf.Reset()
+			pathBuf.Reset()
+		}
 	}
 
 	fmt.Println("generate code success")
